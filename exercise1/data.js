8ce737a96e19f4e8b3728d5c673c1bf23cbc6459
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-09-01 14:12:19 +0700

    adding exercise1

---
+++ exercise1/data.js
@@ -0,0 +1,60 @@
+export let fruits = [
+    {
+        fruitName : "Tomat",
+        latinName : "Solanum lycopersicum",
+        fruitColor: "Merah",
+        fruitBiji : "Dikotil",
+    },
+    {
+        fruitName : "Jeruk",
+        latinName : "Citrus",
+        fruitColor: "Orange",
+        fruitBiji : "Dikotil",
+    },
+    {
+        fruitName : "Pisang",
+        latinName : "Musa sp",
+        fruitColor: "Kuning",
+        fruitBiji : "Monokotil",
+    },
+    {
+        fruitName : "Anggur",
+        latinName : "Vitis vinivera",
+        fruitColor: "Hitam",
+        fruitBiji : "Dikotil",
+    },
+    {
+        fruitName : "Mangga",
+        latinName : "Mangifera indica",
+        fruitColor: "Hijau",
+        fruitBiji : "Dikotil",
+    },
+];
+
+export let animal = [
+    {
+        animalName : "monyet!",
+        latinName  : "Macaca fascicularis",
+        animalSounds: "uuaauuaa"
+    },
+    {
+        animalName : "kucing",
+        latinName  : "Felis catus",
+        animalSounds:"miaw"
+    },
+    {
+        animalName : "ular",
+        latinName  : "Serpentes",
+        animalSounds:"ssssssssssttt"
+    },
+    {
+        animalName : "buaya",
+        latinName  : "Crocodylidae",
+        animalSounds: "hay boleh kenalan gak?"
+    },
+    {
+        animalName : "ayam",
+        latinName  : "Gallus gallus domesticus",
+        animalSounds:"petokpetok"
+    }
+];
